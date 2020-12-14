ebc1ba29752fccbb37961e6f7beab509001d1af4
volha-77 <volha-77@mail.ru>
2020-12-14 19:34:40 +0300

    init: add project HomeWork-3

---
+++ src/Kovaleva.HomeWork-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kovaleva.HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            DateTime start = DateTime.Parse(Console.ReadLine());
+            DateTime end = DateTime.Parse(Console.ReadLine());
+
+            if (end < start)
+            { }
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
