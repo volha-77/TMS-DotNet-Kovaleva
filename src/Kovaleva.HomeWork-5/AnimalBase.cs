acd03ff405ef2b99dd0b6090eac2fd7c068ca5d4
volha-77 <volha-77@mail.ru>
2020-12-25 23:15:16 +0300

    feat: add class AnimalBase

---
+++ src/Kovaleva.HomeWork-5/AnimalBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Kovaleva.HomeWork_5
+{
+    abstract class AnimalBase
+    {
+        public string name { get; set; }
+        public double weight { get; set; }
+        public double age { get; set; }
+
+        public int pawsNumber { get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine($"I am {name}");
+        }
+
+        public void Eat(object meal)
+        {
+            Console.WriteLine($"I eat {meal}");
+        }
+
+        public void Sleap(int duration)
+        {
+            Console.WriteLine("I'm sleaping");
+        }
+    }
+}
---
+++ src/Kovaleva.HomeWork-5/Cat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kovaleva.HomeWork_5
+{
+    public class Cat: AnimalBase
+    {
+    }
+}
