d453097d38459ceb2e09aa0b376615951276a4e6
volha-77 <volha-77@mail.ru>
2021-01-08 14:31:11 +0300

    init: create project, add class Card

---
+++ Kovaleva.HomeWork-6/ATM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kovaleva.HomeWork_6
+{
+    public class ATM
+    {private List<Card>;
+
+    }
+}
---
+++ Kovaleva.HomeWork-6/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kovaleva.HomeWork_6
+{
+    internal class Card
+    {
+        public int CardNumber { get; set; }
+        public int Pin { get; set; }
+        public decimal Balance { get; set; }
+        public string OwnerName { get; set; }
+
+        public Card(int cardNumber, int pin, string ownerName, decimal balance = 0)
+        {
+            CardNumber = cardNumber;
+            Pin = pin;
+            OwnerName = ownerName;
+            Balance = balance;
+        }
+
+        public Card(int cardNumber, int pin)
+        {
+            CardNumber = cardNumber;
+            Pin = pin;
+        }
+
+
+    }
+}
